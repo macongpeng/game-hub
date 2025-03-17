dbf7fa62e5b2dd0ad4e3a837720a20e86a457b86
Denny Ma <macongpeng@gmail.com>
2025-03-17 21:51:42 +1100

    add static genres data back

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,12 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import genresService, { Genre } from "../services/genresService";
-
+import genres from "../data/genres";
 const useGenres = () => useQuery<Genre[], Error>({
     queryKey: ["genres"],
     queryFn: async () => {
         const response = await genresService.getAll().request;
         return response.results;
-    }
+    },
+    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    initialData: genres
 });
 
 export default useGenres;
